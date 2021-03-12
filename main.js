3969e73625dffa6ca9ce9fcdcf8a7124db0a6992
LiminalCrab <devpreston@protonmail.com>
2021-03-12 12:22:54 -0500

    jscript

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-function ringzero(val){
-    let world;
-    world = val;
-    console.log(world);
-}
 
-ringzero('hello world');
+function getFeed(feed){
+    fetch(feed)
+        .then(response => response.text())
+        .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+        .then(data => { 
+        console.log(data)
+        const items = data.querySelectorAll("item");
+        let html = '';
+        items.forEach(el => {
+            html += `
+            <article> 
+                <h2>
+                    <a href="${el.querySelector("link").innerHTML}" target="_blank" rel="noopener">
+                    ${el.querySelector("title").innerHTML}
+                    </a>
+                </h2>
+            </article>`
+        });
+        document.body.insertAdjacentHTML("beforeend", html)
+    });
+}

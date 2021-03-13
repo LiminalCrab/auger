9fd54a2764fcafbbbba172c68d8f4815bca0e878
LiminalCrab <devpreston@protonmail.com>
2021-03-13 16:24:45 -0500

    added the item entry distinction

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 const DOMPARSER = new DOMParser().parseFromString.bind(new DOMParser())
 
-function getSites(){
-    fetch('urls.json')
-    .then(res => res.text())
-    .then(data => {
-        console.log("resolved with:", data);
-        JSON.parse(data).urls.forEach((u => {
-          let url = new URL(`http://127.0.0.1:8080/${u}`); //Temporary solution
-          console.log("parsed from jason:", url); 
-          fetch(url).then((res) => {
-            res.text().then((parsedText) => {
-              let doc = DOMPARSER(parsedText, 'text/xml');
-              console.log(doc)
-          })
-        }) //json 
-    })) //getsites 
+function getData(){
+  fetch('urls.json')
+  .then(response => response.text())
+  .then(data => {
+    console.log("Step one:", data) // del
+    let siteList = JSON.parse(data)
+    console.log("sitelist data", siteList); //del
+
+    //for each, send them to the proxy, and then fetch them again to avoid CORS.
+    siteList.urls.forEach((index => { 
+      let uri = new URL(`http://127.0.0.1:8080/${index}`)
+      console.log(uri);
+
+      //we have to fetch it from the proxy now.
+      fetch(uri).then((res) => {
+        res.text().then((parsedXML) => {
+          let doc = DOMPARSER(parsedXML, 'text/xml')
+          console.log(doc);
+          const item = doc.querySelectorAll("item")
+          const entries = doc.querySelectorAll("entry");
+          console.log(entries, item); // delete
+
+        })
+      })
+    }))
   })
 }

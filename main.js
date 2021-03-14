f484d459b919ee569d7c1911fd6474fc4291d7e3
LiminalCrab <devpreston@protonmail.com>
2021-03-13 23:29:24 -0500

    unix timestamp back to display time

--- main.js
+++ main.js
@@ -1,38 +1,40 @@
 const DOMPARSER = new DOMParser().parseFromString.bind(new DOMParser())
-
+let dateStore = [];
 fetch('urls.json')
 .then(response => response.text())
 .then(data => {
-  //console.log("Step one:", data) // del
   let siteList = JSON.parse(data)
-  //console.log("sitelist data", siteList); //del
 
   //for each, send them to the proxy, and then fetch them again to avoid CORS.
   siteList.urls.forEach((index => { 
     let uri = new URL(`http://127.0.0.1:8080/${index}`)
-    //console.log(uri);
 
     //we have to fetch it from the proxy now.
     fetch(uri).then((res) => {
       res.text().then((parsedXML) => {
         let doc = DOMPARSER(parsedXML, 'text/xml');
-        //console.log(doc);
         const items = doc.querySelectorAll("item"); //surely I don't actually need two of these? 
         const entries = doc.querySelectorAll("entry");
         const innerDate = doc.querySelectorAll("pubDate");
         let html = ``;
-          //html += `<p>${doc.querySelector("published").innerHTML}</p>`
-        //console.log(html);
-        //console.log(entries, items); // delete
+
+
+        //sorting the dates
         innerDate.forEach(el => {
           var dateData = el.innerHTML;
-          el.innerHTML = dateData.replace(/^([0-9]{4})([0-9]{2})([0-9]{2})/, "$1/$2/$3")
+          el.innerHTML = dateData.replace(/^([0-9]{4})([0-9]{2})([0-9]{2})/, "$1/$2/$3") //remove the <> tags
           let parseThedate = Date.parse(dateData);
-          console.log(parseThedate)
-
+          dateStore.push(`${parseThedate}`);
+          dateStore.sort(function(x, y){
+            return y - x;
+          });
+          sortedDate = dateStore.map(function(x){
+            return new Date(x * 1000)
+          })
 
         });
-        
+
+        console.log(sortedDate);
 
         items.forEach(el => {
           html += `

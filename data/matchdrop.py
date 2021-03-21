14dc7462caac0c932b82b27616d74c2e560bcd88
LiminalCrab <devpreston@protonmail.com>
2021-03-21 03:24:10 -0400

    matching

--- data/matchdrop.py
+++ data/matchdrop.py
@@ -1,17 +1,36 @@
 import asyncio
 import psycopg2
 
-#open initial connection
-conn = psycopg2.connect(
-    host="",
-    database="",
-    user="",
-    password="",
-    port=5432 )
 
-#open initial cursor
-cur = conn.cursor()
-cur.close()
-conn.close()
+async def main():
+    #open initial connection
+    conn = psycopg2.connect(
+        host="",
+        database="",
+        user="",
+        password="",
+        port=5432 )
 
+    #open initial cursor
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM posts;')
+    results = cur.fetchall()
+    for r in results:
+        print(f"{r[0]} and {r[2]}")
+        
+    dupes_del = '''
+    DELETE FROM posts
+    WHERE id IN (SELECT id,
+                        ROW_NUMBER() OVER (partition BY
+                        host_title, post_url, post_date ORDER BY id)
+                        AS rnum FROM posts) t
+                        WHERE t.rnum > 1);
+    '''
+    cur.execute(dupes_del)
+    
+    cur.close()
+    conn.close()
+
+if __name__ == '__main__':
+    asyncio.run(main())  
 

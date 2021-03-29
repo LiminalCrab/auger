d8232f8aa9cfbbc82d605a0606c6fcb7e24805c6
LiminalCrab <devpreston@protonmail.com>
2021-03-29 14:56:36 -0400

    fetching

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const fs = require('fs')
 
-
 fs.readFile('./data/links.json', 'utf8', (err, fsToString) => {
     let data = JSON.parse(fsToString);
-    console.log(data.map(link => link.url))
-})
+    console.log(data.map(link => console.log(link[0].date, link[0].url, link[0].title)))
+});

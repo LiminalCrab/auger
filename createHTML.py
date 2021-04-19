8e5f9bc52fc5be9edbbe8635145c4af9ce4cb02c
LiminalCrab <devpreston@protonmail.com>
2021-04-19 02:24:18 -0400

    removed javascript.

--- createHTML.py
+++ createHTML.py
@@ -1,4 +1,5 @@
-from jinja2 import Environment, FileSystemLoader 
+from jinja2 import Environment, FileSystemLoader
+from datetime import datetime
 import os
 import json
 import psycopg2
@@ -14,38 +15,31 @@ def loadTemplate():
     env = Environment(loader = FileSystemLoader(templates_dir))
     template = env.get_template('template')
     return template
-
+    
 def loadData():
+# Let's grab some data from postgres
     try:
-        #we want ALL of the data. ALL OF IT.
-        get_post_url = ''' 
-        SELECT post_url FROM posts;
-        '''
-        get_host_title = '''
-        SELECT host_title FROM posts;
-        '''
-        get_post_date = '''
-        SELECT post_date FROM posts;
-        '''
-    except KeyError:
-        print("ERROR")
         
-    #Let's depreciate.
+        q_select = '''
+        SELECT host_title, post_url, to_char(post_date, 'DD Mon YYYY') 
+        FROM posts ORDER BY post_date DESC;
+        '''
+        cur.execute(q_select)
+        origin_data = [cur.fetchall()]
+        
+    except:
+        
+        print("EXCEPTION THROWN")
+        print("ORIGIN DATA", origin_data)
+        print("Are you connected to a database?")
+        
+    for row in origin_data:
+        
+        processed_data = [x for x in row]
+        
+        return processed_data
     
-    
-    ''' 
-        aim to deprecate this function or load directly from psycog2
-
-    try:
-        # try an environment var
-        data_file = os.environ['data']
-    except KeyError:
-        # defaul to already existing location
-        data_file = 'data/links.json'
-    with open(data_file, 'r') as dfr:
-         data_file = json.load(dfr)
-    return data_file
-'''
+        
 def makeHTML(template, data):
     filename = os.path.join(os.getcwd(), 'index.html')
     with open(filename, 'w+') as fw:

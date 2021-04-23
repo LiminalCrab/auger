1f60ba4283f80ec9b785e0f34b4de6242f23a9da
LiminalCrab <devpreston@protonmail.com>
2021-04-23 01:50:08 -0400

    saving for revert" "

--- createHTML.py
+++ createHTML.py
@@ -45,18 +45,27 @@ def makeHTML(template, data):
     for x in range(len(data)):
         max_posts = x #1646
         
-    current_post = 0 #current post number
-    pg_num = 0 #page number used for file names
-    page_limit = 100 #when the data reaches this number, we want to create a new file and then continue writing starting from
-    #the current post?
+    current_post = 1 #current post number
+    pg_num = 1 #page number used for file names
+    post_limit = 100 #when the data reaches this number, we want to create a new file and then continue writing starting from
+    quotient = max_posts // post_limit
     filename = os.path.join(os.getcwd(), 'index.html')
     with open(filename, 'w+') as fw:
+        fw.write(template.render(data=data[:current_post]))
+        fw.close()
         for article in data:
-                current_post += 1
-                pg_num += 1
-                if current_post < page_limit: #if post limit is less than the length of data
-                    fw = open('page/%s.html' % pg_num, 'w')
-                    fw.write(template.render(data=data[:current_post]))
+            print("Article", article)
+            print("current_post:", current_post)
+            print("pg_num", pg_num)
+            print("quotient:", quotient)
+
+                #if current_post < post_limit: #if post limit is less than the length of data
+                    #after writing to index, start creating numbered pages.
+                    #fw = open('page/%s.html' % pg_num, 'w')
+                    #fw.write(template.render(data=data[:2]))
+                    
+            current_post += 1
+            pg_num += 1
                 
    
     #filename = os.path.join(os.getcwd(), 'index.html')
